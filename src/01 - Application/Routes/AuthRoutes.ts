99d2211cb9bbd1c76c890df4663db5ce69c61f99
Felipe Vaz <felipevaz@live.com>
2024-10-18 20:31:40 -0300

    correção na rota e suas dependências

--- src/01 - Application/Routes/AuthRoutes.ts
+++ src/01 - Application/Routes/AuthRoutes.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import { AuthController } from '../Controllers/AuthController';
-import { AuthService } from '../../03 - Service/AuthService';
-import { UserRepository } from '../../04 - Infrastructure/4.1 - Data/Repository/UserRepository';
 
 const router = express.Router();
 
 // Instanciar o UserService e o AuthController
-const userRepository = new UserRepository();
-const authService = new AuthService(userRepository);
-const authController = new AuthController(authService);
+const authController = new AuthController();
 
 // Rota de login
 router.post('/auth/login', authController.login.bind(authController));
